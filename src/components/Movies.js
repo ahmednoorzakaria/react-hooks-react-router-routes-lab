7fcab7074f42fd499fd18fed97514deabb795c43
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-07-28 17:33:32 +0300

    finished

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,31 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map(
+        (
+          data 
+        ) => (
+          <div key={data.title}>
+            {" "}
+            <p>{data.title}</p>
+            <p>{data.time}</p>
+            <ul>
+              {data.genres.map(
+                (
+                  genre 
+                ) => (
+                  <li key={Math.random()}>{genre}</li> 
+                )
+              )}
+            </ul>
+          </div>
+        )
+      )}
+    </div>
+  );
 }
 
 export default Movies;
